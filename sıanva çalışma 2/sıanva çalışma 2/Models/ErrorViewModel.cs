3554dfa0fabe8532aab529913b8a8cb8a59f9da7
Eastsoul1346 <122230261+Eastsoul1346@users.noreply.github.com>
2023-01-08 23:56:47 +0300

    123
    12

---
+++ sıanva çalışma 2/sıanva çalışma 2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sıanva_çalışma_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
